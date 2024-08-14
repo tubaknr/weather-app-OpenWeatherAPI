047b731ee36cfd6df6c0e1c3dbec140947ce903a
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-08-14 21:17:17 +0300

    first

---
+++ main.js
@@ -0,0 +1,44 @@
+const apiKey = "d92dda73d466e16f2f5853ee138cbea3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    
+    console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "imgs/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "imgs/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "imgs/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "imgs/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "imgs/mist.png";
+    }
+}
+
+searchBtn.addEventListener('click', ()=> {
+    checkWeather(searchBox.value);
+})
+
+
+
+
+
+
+
